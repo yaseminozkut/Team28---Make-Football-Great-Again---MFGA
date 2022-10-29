778d77f3e129591d604f5ea5e5ed9b4bdeb89e4e
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-10-29 18:22:21 +0300

    login page
    login page using react

---
+++ Frontend/mfga-front/src/components/Login/login.jsx
@@ -0,0 +1,71 @@
+//Alper Kaan
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+export const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    const registered = {
+      ...values,
+      [name]: value,
+    };
+
+    axios
+      .post("http://localhost:4000/login", registered)
+      .then((response) => console.log(response.data));
+
+    setValues({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="form-div">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={handleChange}
+              name="email"
+              value={values.email}
+              className="form-control form-group"
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              name="password"
+              value={values.password}
+              className="form-control form-group"
+            />
+
+            <input
+              type="submit"
+              className="btn btn-danger btn-block"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
