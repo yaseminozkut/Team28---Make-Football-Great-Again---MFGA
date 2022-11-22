47ab7818bfcd5a35d2237e0448ddc43ff79c3f8d
yaseminozkut <ozkutyasemin@gmail.com>
2022-11-22 15:06:57 +0300

    Change Footer
    Changed elements in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Wrapper, Row, Column, Link, Title } from './footerElements'
+import { Container, Wrapper, Row, Column, Link, Title, Title2, Text } from './footerElements'
 
 
 export function Footer() {
@@ -8,38 +8,17 @@ export function Footer() {
             <Wrapper>
             <Row>
                 <Column>
-                <Title>About Us</Title>
-                    <Link href="#">Story</Link>
-                    <Link href="#">Contact</Link>
-                    <Link href="#">Terms & Condition</Link>
+                <Title>Go To</Title>
+                    <Link href="/">Homepage</Link>
+                    <Link href="currentstats">Current Statsistics</Link>
+                    <Link href="referee">Referees</Link>
                 </Column>
                 <Column>
-                <Title>Title2</Title>
-                    <Link href="#">Marketing</Link>
-                    <Link href="#">Consulting</Link>
-                    <Link href="#">Development</Link>
-                    <Link href="#">Design</Link>
-                </Column>
-                <Column>
-                <Title>Contact Us</Title>
-                    <Link href="#">United States</Link>
-                    <Link href="#">United Kingdom</Link>
-                    <Link href="#">Australia</Link>
-                    <Link href="#">Support</Link>
-                </Column>
-                <Column>
-                <Title>Contact Us</Title>
-                    <Link href="#">United States</Link>
-                    <Link href="#">United Kingdom</Link>
-                    <Link href="#">Australia</Link>
-                    <Link href="#">Support</Link>
-                </Column>
-                <Column>
-                <Title>Contact Us</Title>
-                    <Link href="#">United States</Link>
-                    <Link href="#">United Kingdom</Link>
-                    <Link href="#">Australia</Link>
-                    <Link href="#">Support</Link>
+                <Title2>Make Football Great Again</Title2>
+                <Text>Welcome to our Make Football Great Again community. 
+                Here, you can find useful statistics and news about Turkish Football. 
+                If you have any questions or suggestions feel free to contact us.
+                </Text>
                 </Column>
             </Row>
             </Wrapper>
