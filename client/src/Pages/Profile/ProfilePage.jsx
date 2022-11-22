1817d7563ca33f3c9b705c52830ab590fdb2d6ae
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-11-22 21:55:51 +0300

    profile page is updated

--- client/src/Pages/Profile/ProfilePage.jsx
+++ client/src/Pages/Profile/ProfilePage.jsx
@@ -1,112 +1,121 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {DropDownList,standingsTitle,DropDownListContainer,UsernameTitle, ContainerCard, NameTitle, ContainerDiv,StyledButton,StyledNavLink,TeamTitle, ListItem,DropDownContainer,DropDownHeader, StandingCard} from "./ProfileElements";
-import {useNavigate, useLocation} from 'react-router-dom';
+import {
+  DropDownList,
+  standingsTitle,
+  DropDownListContainer,
+  UsernameTitle,
+  ContainerCard,
+  NameTitle,
+  ContainerDiv,
+  StyledButton,
+  StyledNavLink,
+  TeamTitle,
+  ListItem,
+  DropDownContainer,
+  DropDownHeader,
+  StandingCard,
+} from "./ProfileElements";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Footer } from "../../components/Footer/Footer";
-import {Loading} from "../../components/Loading/loading"
+import Loading from "../../components/Loading/loading";
 
+export const Profile = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-export const Profile=()=>{
-    const location = useLocation();
-const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const email = user.email;
+  var team = user.team;
 
-const user = JSON.parse(localStorage.getItem("currentUser"));
+  const [u_teams, SetUteams] = useState([]);
 
-if(user === null) {
-  return <Loading></Loading>
-}
-
-const email = user.email; 
-var team = user.team;
-
-    const [u_teams,SetUteams] = useState([]);
-
-    
-    axios.get('https://mfga.herokuapp.com/profile')
-    .then(res =>{
-        const u_teams = res.data;
-        SetUteams(u_teams)
-        console.log(u_teams)
+  axios
+    .get("https://mfga.herokuapp.com/profile")
+    .then((res) => {
+      const u_teams = res.data;
+      SetUteams(u_teams);
+      console.log(u_teams);
     })
-    .catch((err)=>{
-        console.log(err);
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-
-
-
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     navigate("/edit");
   };
-  
-    
- 
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState(null);
-    const toggling = () => setIsOpen(!isOpen);
 
-    const onOptionClicked = value => () => {
-        setSelectedOption(value);
-        setIsOpen(false);
-        const Selectedteam ={
-            team:value,
-            email:email
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const toggling = () => setIsOpen(!isOpen);
 
-        }
-        console.log(Selectedteam)
-        axios
-        .post("https://mfga.herokuapp.com/profile", Selectedteam)
-        .then((response) => {
-            window.alert("User successfully selected team")
-            
-          
-            localStorage.setItem("currentUser", JSON.stringify({name: response.data.user.name, username: response.data.user.username, email: response.data.user.email, password: response.data.user.password,team: response.data.user.team}))
-            localStorage.setItem("user", response.data.user);
+  const onOptionClicked = (value) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    const Selectedteam = {
+      team: value,
+      email: email,
+    };
+    console.log(Selectedteam);
+    axios
+      .post("https://mfga.herokuapp.com/profile", Selectedteam)
+      .then((response) => {
+        window.alert("User successfully selected team");
 
-        });
+        localStorage.setItem(
+          "currentUser",
+          JSON.stringify({
+            name: response.data.user.name,
+            username: response.data.user.username,
+            email: response.data.user.email,
+            password: response.data.user.password,
+            team: response.data.user.team,
+          })
+        );
+        localStorage.setItem("user", response.data.user);
+      });
+  };
 
-      
+  if (user === null) {
+    return <Loading></Loading>;
+  }
 
-      };
-
-return(
+  return (
     <ContainerDiv>
-    <ContainerCard>
-    
+      <ContainerCard>
         <NameTitle>{user.name}</NameTitle>
         <TeamTitle>My Team</TeamTitle>
         <UsernameTitle>{user.username}</UsernameTitle>
         <DropDownContainer>
-        <DropDownHeader onClick={toggling}>{user.team||"Select Team"}</DropDownHeader>
-        {isOpen && (
-          <DropDownListContainer>
-            <DropDownList>
-            {u_teams.map(option => (
-                <ListItem onClick={onOptionClicked(option.name)} key={Math.random()}>
-                  {option.name}
-                </ListItem>
-              ))}
-            </DropDownList>
-          </DropDownListContainer>
-        )}
-      </DropDownContainer>
+          <DropDownHeader onClick={toggling}>
+            {user.team || "Select Team"}
+          </DropDownHeader>
+          {isOpen && (
+            <DropDownListContainer>
+              <DropDownList>
+                {u_teams.map((option) => (
+                  <ListItem
+                    onClick={onOptionClicked(option.name)}
+                    key={Math.random()}
+                  >
+                    {option.name}
+                  </ListItem>
+                ))}
+              </DropDownList>
+            </DropDownListContainer>
+          )}
+        </DropDownContainer>
         <StyledNavLink active href="/edit">
-            Edit
-          </StyledNavLink>
-          <StandingCard>
+          Edit
+        </StyledNavLink>
+        <StandingCard>
           <standingsTitle>Rank:</standingsTitle>
           <standingsTitle>Win:</standingsTitle>
           <standingsTitle>Lose:</standingsTitle>
           <standingsTitle>Draw:</standingsTitle>
           <standingsTitle>Points:</standingsTitle>
-
-
-
-            
-          </StandingCard>
-    </ContainerCard>
+        </StandingCard>
+      </ContainerCard>
     </ContainerDiv>
-)
-
-
-}
+  );
+};
