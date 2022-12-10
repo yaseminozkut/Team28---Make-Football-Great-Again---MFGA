91d5d28e69a29dc282e952f6adbb0b9ccd7c3d8b
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2022-12-10 13:07:08 +0300

    push

--- client/src/Pages/Referee/RefereeElements.js
+++ client/src/Pages/Referee/RefereeElements.js
@@ -67,3 +67,40 @@ export const Title = styled.h1`
   font-size: 2.18rem;
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 `;
+
+export const DropDownContainer2 = styled("div")`
+  width: 10.5em;
+  margin: 0 auto;
+`;
+
+export const DropDownHeader2 = styled("div")`
+  margin-bottom: 0.8em;
+  padding: 0.4em 2em 0.4em 1em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  font-weight: 500;
+  font-size: 1.3rem;
+  color: #3faffa;
+  background: #ffffff;
+`;
+
+export const DropDownListContainer2 = styled("div")``;
+
+export const DropDownList2 = styled("ul")`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+export const ListItem2 = styled("li")`
+  list-style: none;
+  margin-bottom: 0.8em;
+`;
--- client/src/Pages/Referee/referee.js
+++ client/src/Pages/Referee/referee.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import { ContainerDiv, Title } from "./RefereeElements";
+import { ContainerDiv, Title,DropDownContainer2,DropDownHeader2,DropDownList2,ListItem2,DropDownListContainer2 } from "./RefereeElements";
 import { Footer } from "../../components/Footer/Footer";
 import { RefereeCard } from "../../components/RefereeCard/refereeCard";
 
 export const Referee = (props) => {
   const [referees, SetReferees] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [1, 2, 3,4,5];
+  const toggling = () => setIsOpen(!isOpen);
 
   useEffect(() => {
     axios
@@ -34,11 +38,41 @@ export const Referee = (props) => {
       />
     );
   }
-
+  
+  
+  const onOptionClicked = value => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    console.log(selectedOption);
+   
+  };
+  
   return (
     <ContainerDiv>
+
     <Title>Referees</Title>
-      {referees.map(createCard)}
+    
+      
+    {referees.filter((a)=> a.name === "Arda Kardesler" ).map(createCard)}
+
+      
+      <DropDownContainer2>
+        <DropDownHeader2 onClick={toggling}>
+          {selectedOption||"Select a number to see higher rated referees"}
+        </DropDownHeader2>
+        {isOpen && (
+          <DropDownListContainer2>
+            <DropDownList2>
+              {options.map(option => (
+                <ListItem2 onClick={onOptionClicked(option)} key={Math.random()}>
+                  {option}
+                </ListItem2>
+              ))}
+            </DropDownList2>
+          </DropDownListContainer2>
+        )}
+      </DropDownContainer2>
+       {referees.filter((a)=> a.rating === "5" ).map(createCard)}
       <Footer></Footer>
     </ContainerDiv>
   );
