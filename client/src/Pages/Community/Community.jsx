f9a302f6915fd931f2f3fc6562f7cc0a4521c4dc
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-12-11 16:08:12 +0300

    Community Page
    Community Page is implemented via custom styling, post button, post send option, custom post cards and react-pro-sidebar

--- client/src/Pages/Community/Community.jsx
+++ client/src/Pages/Community/Community.jsx
@@ -1,8 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+import {
+  CustomSidebar,
+  PostButton,
+  PostContainer,
+  GlobalContainer,
+  CustomContainer,
+} from "./CommunityElements";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { PostCard } from "../../components/PostCard/PostCard";
+import Loading from "../../components/Loading/loading";
+import { PostForm } from "../../components/PostForm/PostForm";
+
 
 export const Community = () => {
-  return (
-    <div>Alper</div>
-  )
-}
 
+  const [posts, SetPosts] = useState([]);
+  const [isForm, setForm] = useState(false);
+
+  console.log(isForm);
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:4000/api/getAllPost")
+        .then( (res) => {
+          const posts = res.data;
+          SetPosts(posts);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, []);
+
+
+
+  if (posts === 0) {
+    return <Loading></Loading>;
+  }
+
+
+  const CreatePostHandler = () => {
+      setForm(!isForm)
+  }
+
+
+  function createCard(post) {
+    return (
+      <PostCard
+        key={post._id}
+        name={post.postedBy.name}
+        content={post.content}
+        likeCount={post.likeCount}
+        commentCount={post.commentCount}
+      />
+    );
+  }
+
+  // console.log(isForm)
+
+  return (
+    <ProSidebarProvider>
+      { !isForm ?  <GlobalContainer>
+        <CustomContainer>
+          <CustomSidebar>
+            <Menu>
+              <SubMenu label="Filter by comment count">
+                <MenuItem> more than 30 </MenuItem>
+                <MenuItem> more than 20 </MenuItem>
+                <MenuItem> more than 10 </MenuItem>
+                <MenuItem> more than 5 </MenuItem>
+              </SubMenu>
+              <SubMenu label="Filter by like count">
+                <MenuItem> more than 30 </MenuItem>
+                <MenuItem> more than 20 </MenuItem>
+                <MenuItem> more than 15 </MenuItem>
+                <MenuItem> more than 10 </MenuItem>
+              </SubMenu>
+
+              <PostButton onClick = {CreatePostHandler} >Create Post</PostButton>
+            </Menu>
+          </CustomSidebar>
+        </CustomContainer>
+
+        
+        <PostContainer>
+          {[...posts]
+              .sort((a, b) => (a.rank < b.rank ? 1 : -1))
+              ?.map(createCard)}
+        </PostContainer>
+      </GlobalContainer> : <PostForm isForm={isForm} />}
+      
+    </ProSidebarProvider>
+  );
+};
