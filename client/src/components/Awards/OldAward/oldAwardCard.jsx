65529e1775c8be7ca33d74d85158ccfa9e5dec80
yaseminozkut <ozkutyasemin@gmail.com>
2022-12-13 22:32:04 +0300

    Create old award card
    Old award card is created for old awards page

--- client/src/components/Awards/OldAward/oldAwardCard.jsx
+++ client/src/components/Awards/OldAward/oldAwardCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Card, CardBody, CardText, CardTitle, ListGroup, ListGroupItem } from "reactstrap";
 
-export const ActiveAwardCard = ({props}) => {
+export const OldAwardCard = (props) => {
   return (
     <Card
       style={{
         width: "23rem",
-        height: "35rem",
+        height: "35rem"
       }}
     >
       <img alt="Card" src={props.image} />
@@ -16,6 +16,7 @@ export const ActiveAwardCard = ({props}) => {
       </CardBody>
       <ListGroup flush>
         <ListGroupItem>Points Needed: {props.pointsNeeded}</ListGroupItem>
+        <ListGroupItem>Winner: {props.winner}</ListGroupItem>
       </ListGroup>
     </Card>
   );
