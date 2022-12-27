b60c705efa4f441429667984cc8a2ba466c359fa
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-12-27 16:15:26 +0300

    Add twitter Api Keys to config
    Twitter api keys are added into config/api.js file

---
+++ config/api.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'Bed6TSFzQIKP4VUKfi8NqSIaz',
+    consumer_secret: 'eZIIhVGYCmGTtBpbrf9Niu3braWLu6OWU8I7K0TBXhmWKWFvTu',
+    access_token_key: '1490639250991292418-qOTzF1jqhy9FxwghLkRs2pjM5mP2Dy',
+    access_token_secret: 'xcXTy3B7omroocxKzpK99zyhIP0yYy9TW9nx0xLpXa44a'
+}
