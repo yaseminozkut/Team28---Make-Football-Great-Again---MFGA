d6e3f3cba642289352a6099968c1233d0cdbc7dc
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-12-27 16:23:23 +0300

    api/news route is created
    api/news route is created in backend for retrieving recent news
    about TFF using twitter and showNews controller function implemented in
    ./controllers/news folder

--- routes/news/newsRoute.js
+++ routes/news/newsRoute.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const NewsController = require("../../controllers/news");
+
+router.get('/news', NewsController.showNews);
+
+
+module.exports = router;
