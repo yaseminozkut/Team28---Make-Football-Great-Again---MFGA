d4f5442f60f82a1929ed48e8fbb499855c7de11a
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-12-27 16:27:17 +0300

    News button and News are implemented
    News button and news coming from api route with useEffect hook is integrated into homepage

--- client/src/Pages/Homepage/homepage.jsx
+++ client/src/Pages/Homepage/homepage.jsx
@@ -1,29 +1,48 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Menu, ProSidebarProvider, SubMenu } from "react-pro-sidebar";
+import { Button } from "reactstrap";
 import { Footer } from "../../components/Footer/Footer";
 import Loading from "../../components/Loading/loading";
-
+import { NewsCard } from "../../components/News/NewsCard";
 
 import {
   ContainerDiv,
+  CustomHeaderTFFNews,
+  CustomImage,
+  CustomImageTFF,
+  CustomSidebar,
   Fixture,
   InnerContainer,
+  NewsContainer,
   RecentMatchButton,
+  TFFButton,
   UpcomingMatch,
   UpComingMatchContainer,
 } from "./HomepageElements";
 
 const Homepage = () => {
-
-  const [recentMatch, setRecentMatch] = useState([])
+  const [recentMatch, setRecentMatch] = useState([]);
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:4000/recentmatch/getrecent");
+      const result = await axios.get(
+        "http://localhost:4000/recentmatch/getrecent"
+      );
 
       setRecentMatch(result.data);
     };
 
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("http://localhost:4000/api/news");
+
+      setNews(result.data);
+    };
 
     fetchData();
   }, []);
@@ -32,8 +51,12 @@ const Homepage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState("");
 
-  if(recentMatch.length === 0) {
-    return <Loading></Loading>
+  if (recentMatch.length === 0) {
+    return <Loading></Loading>;
+  }
+
+  if (news.length === 0) {
+    return <Loading></Loading>;
   }
   // console.log(recentMatch)
 
@@ -42,7 +65,9 @@ const Homepage = () => {
 
     try {
       const response = await fetch(
-        "https://mocki.io/v1/08c6b2bd-5741-4f67-af9f-7f3e1e981ee5",
+        // "https://mocki.io/v1/08c6b2bd-5741-4f67-af9f-7f3e1e981ee5",
+        "https://mocki.io/v1/4af3e014-9836-4c73-87e2-ae73ddcd0b82",
+        // 'https://api.collectapi.com/sport/results?data.league=super-lig',
         {
           method: "GET",
           headers: {
@@ -57,7 +82,9 @@ const Homepage = () => {
         throw new Error(`Error! status: ${response.status}`);
       }
 
-      const result = await response.json();
+      var result = await response.json();
+
+      // result = result["result"] //If YOU USE COLLECT API USE THIS COMMAND
 
       console.log("result is: ", JSON.stringify(result, null, 4));
 
@@ -69,28 +96,27 @@ const Homepage = () => {
       }
 
       setData(result);
+      console.log("result is: " + result);
 
-      console.log(result.length)
-      for(var i = 0 ; i < result.length ; i++) {
-
+      console.log(result.length);
+      for (var i = 0; i < result.length; i++) {
         // console.log(result[i])
         var currentData = result[i];
-        console.log(currentData)
+        console.log(currentData);
 
-        axios.post('http://localhost:4000/recentmatch/recent', currentData)
-        .then((res) => {
-          if(res.status === 200) {
-            console.log(res.data)
-          }else {
-            console.log("Already exists");
-          }
-        }).catch((e) => {
-          console.log(e);
-        })
-
-       }
-
-
+        axios
+          .post("http://localhost:4000/recentmatch/recent", currentData)
+          .then((res) => {
+            if (res.status === 200) {
+              console.log(res.data);
+            } else {
+              console.log("Already exists");
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
     } catch (err) {
       setErr(err.message);
     } finally {
@@ -99,28 +125,59 @@ const Homepage = () => {
   };
 
   console.log(data);
+  console.log(news);
 
   return (
     <>
-      {err && <h2>{err}</h2>}
-      {isLoading && <h2>Loading...</h2>}
-      <ContainerDiv>
-        <InnerContainer>
-          <UpComingMatchContainer>
-            <Fixture>Recent matches</Fixture>
-            {recentMatch &&
-              recentMatch.map((r) => (
-                <UpcomingMatch key={r.home}>
-                  {r.date} <br /> {r.away} - {r.home} <br/> {r.skor}
-                </UpcomingMatch>
+      <ProSidebarProvider>
+        {err && <h2>{err}</h2>}
+        {isLoading && <h2>Loading...</h2>}
+        <ContainerDiv>
+          <InnerContainer>
+            {/* <CustomImage
+              src="https://i.pinimg.com/originals/b2/7c/2d/b27c2d3cfbc89a37042cdc5a01be60c4.png"
+              alt="PageImage"
+            ></CustomImage> */}
+
+            <NewsContainer>
+              {[...news].map((n) => (
+                <NewsCard key={n.id} full_text={n.full_text} />
               ))}
+            </NewsContainer>
 
-              <RecentMatchButton onClick={handleClick}> Refresh Feed </RecentMatchButton>
-          </UpComingMatchContainer>
+            <CustomSidebar>
+              <CustomHeaderTFFNews>TFF NEWS</CustomHeaderTFFNews>
+              <>
+                <TFFButton>
+                  <CustomImageTFF
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Turkish_Football_Federation_crest.svg/1200px-Turkish_Football_Federation_crest.svg.png"
+                    alt="my image"
+                    // onClick={this.myfunction}
+                  />
+                </TFFButton>
+              </>
+            </CustomSidebar>
 
-        </InnerContainer>
-        <Footer></Footer>
-      </ContainerDiv>
+            <UpComingMatchContainer>
+              <Fixture>Recent matches</Fixture>
+              {recentMatch &&
+                [...recentMatch]
+                  .sort((a, b) => (a.date < b.date ? 1 : -1))
+                  ?.map((r) => (
+                    <UpcomingMatch key={r.home}>
+                      {r.date} <br /> {r.away} - {r.home} <br /> {r.skor}
+                    </UpcomingMatch>
+                  ))}
+
+              <RecentMatchButton onClick={handleClick}>
+                {" "}
+                Refresh Feed{" "}
+              </RecentMatchButton>
+            </UpComingMatchContainer>
+          </InnerContainer>
+          <Footer></Footer>
+        </ContainerDiv>
+      </ProSidebarProvider>
     </>
   );
 };
