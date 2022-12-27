739f42f3f4bb992502f0b5dddc355748dd29ef35
yaseminozkut <ozkutyasemin@gmail.com>
2022-12-27 21:30:14 +0300

    Update localhost port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:4000",
+  origin: "http://localhost:3000",
   credentials: true,
 }));
 
