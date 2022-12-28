a6a5be011214d47e4e7689123e41dc34b443d9d7
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2022-12-28 19:37:31 +0300

    change the profile page and text
    Change the profile page and text

--- client/src/components/TeamPost/TeamPostCard.jsx
+++ client/src/components/TeamPost/TeamPostCard.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap'
-import { CustomCard, TFFAvatar, CustomCardText } from './TeamPostCardElements'
-
-
+import React from "react";
+import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import { CustomCard, TFFAvatar, CustomCardText } from "./TeamPostCardElements";
 
 export const TeamPostCard = (props) => {
   return (
     <CustomCard>
-
-      <TFFAvatar src="https://pbs.twimg.com/profile_images/1333774609494241281/BPF9ei6K_400x400.jpg" alt="Tff Logo" />
+      <TFFAvatar
+        src="https://pbs.twimg.com/profile_images/1583812988913549312/AMcNHO2m_400x400.jpg"
+        alt="Tff Logo"
+      />
 
       <CustomCardText>
-        HI
+        Nef Stadyumu’nda tüyleri diken diken eden o muhteşem koreografi! 😍 “Ey
+        yükselen yeni nesil, İstikbal sizindir. Cumhuriyeti biz kurduk, onu
+        yüceltecek ve yaşatacak sizlersiniz.” İLELEBET İZİNDEYİZ ATAM! 🇹🇷
+        #Cumhuriyetin100üGalatasaray
       </CustomCardText>
-
     </CustomCard>
-
-  )
-}
+  );
+};
