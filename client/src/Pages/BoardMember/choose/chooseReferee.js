bac1d6baed6fcad1fedb8e0f33e0c30029b077e9
alivehbi <alivehbi@sabanciuniv.edu>
2023-01-02 14:36:57 +0300

    Match and Referee assignment
    Referee assignment algorithm changes

--- client/src/Pages/BoardMember/choose/chooseReferee.js
+++ client/src/Pages/BoardMember/choose/chooseReferee.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import { ContainerDiv,FilterTitle, Title,DropDownContainer2,DropDownHeader2,DropDownList2,ListItem2,DropDownListContainer2, StyledBackButton } from "./chooseRefereeElements";
+import { ContainerDiv,FilterTitle, Title ,StyledBackButton } from "./chooseRefereeElements";
 import { Footer } from "../../../components/Footer/Footer";
 import { RefereeCard } from "./chooseCard";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,useParams } from "react-router-dom";
 export const ChooseReferee = (props) => {
+  const {home,away} = useParams();
+  var MatchImportance = 0;
+  var MatchImportanceStr = "";
+
+
   const navigate = useNavigate();
   const [referees, SetReferees] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
-  const options = [0,1, 2, 3,4,5];
-  const toggling = () => setIsOpen(!isOpen);
+  const [teamStat, setTeamStat] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("http://localhost:4000/stat/getStat");
+      setTeamStat(result.data);
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     axios
@@ -37,7 +47,8 @@ export const ChooseReferee = (props) => {
       <RefereeCard
         key={referee._id}
         name={referee.name}
-        rating ={CalculateRating(referee)}
+        rating ={CalculatePoint(referee)}
+        
       />
     );
   }
@@ -52,45 +63,146 @@ export const ChooseReferee = (props) => {
     }
 
   }
+  function CalculateYellow(referee){
+    const myAverage = (Math.round((referee.yellowCard/referee.matchCount)* 100) / 100).toFixed(2);
 
-  
-  const onOptionClicked = value => () => {
-    setSelectedOption(value);
-    setIsOpen(false);
-    console.log(selectedOption);
+    if (myAverage <= 4.5) {
+      return 5;
+    }
+    else if(myAverage > 4.5 && myAverage <= 5){
+      return 3;
+    }
+    else {
+      return 1;
+    }
+
+  }
+  function CalculateRed(referee){
+    const myAverage = (Math.round((referee.redCard/referee.matchCount)* 100) / 100).toFixed(2);
+
+    if (myAverage <= 0.2) {
+      return 5;
+    }
+    else if(myAverage > 0.2 && myAverage <= 0.4){
+      return 3;
+    }
+    else {
+      return 1;
+    }
+
+  }
+  function CalculateYellowRed(referee){
+    const myAverage = (Math.round((referee.yellowRedCard/referee.matchCount)* 100) / 100).toFixed(2);
+
+    if (myAverage <= 0.15) {
+      return 5;
+    }
+    else if(myAverage > 0.15 && myAverage <= 0.3){
+      return 3;
+    }
+    else {
+      return 1;
+    }
+
+  }
+  function CalculatePenalty(referee){
+    const myAverage = (Math.round((referee.penalty/referee.matchCount)* 100) / 100).toFixed(2);
+
+    if (myAverage <= 0.4) {
+      return 5;
+    }
+    else if(myAverage > 0.4 && myAverage <= 0.55){
+      return 3;
+    }
+    else {
+      return 1;
+    }
+
+  }
+  function CalculateRank(home, away){
+    var HomeAwayRank = 0
+    var pointArray = [10,10,10,9,9,8,8,5,5,3,3,3,2,2,2,1,1,1,1]
+    
+    teamStat.forEach(stat => {
+      if(stat.team === home)
+      {
+        HomeAwayRank += pointArray[stat.rank -1]
+
+      }
+      if(stat.team === away){
+        HomeAwayRank += pointArray[stat.rank -1]
+
+      }
+    });
+    console.log(HomeAwayRank)
+    if (HomeAwayRank <= 20 && HomeAwayRank >= 15) {
+      
+      return 75;
+      
+    }
+    else if (HomeAwayRank <= 14 && HomeAwayRank >= 10) {
    
-  };
-  
+
+      return 60;
+    }
+    else if (HomeAwayRank <= 9 && HomeAwayRank >= 5) {
+   
+      return 50;
+    }
+    else{
+     
+      return 0;
+    }
+  }
+  function CalculatePoint(referee){
+    MatchImportance = CalculateRank(home,away)
+    const rating = ((CalculateRating(referee) * 20)* 80) /100;
+    const redCard = CalculateRed(referee)
+    const yellowCard = CalculateYellow(referee)
+    const yellowRedCard = CalculateYellowRed(referee)
+    const penalty = CalculatePenalty(referee)
+    const totalPoint = rating + redCard + yellowCard + yellowRedCard + penalty
+    return totalPoint;
+  }
+  function Importance(ImpStr,ImpInt) {
+    
+  }
+
   return (
     
     <ContainerDiv>
     <StyledBackButton onClick={handleBack}>{"<"}Back</StyledBackButton>
 
     <Title>Referees</Title>
-    <FilterTitle>Filter Rating</FilterTitle>
     
-    
-    {referees.filter((referee)=> CalculateRating(referee) >= selectedOption).map(createCard)}
+    {referees.filter((referee)=> {
+      if(CalculateRank(home,away) === 75){
+        MatchImportance = 75;
+        MatchImportanceStr = "Very High Importance";
 
-      
-      <DropDownContainer2>
-        <DropDownHeader2 onClick={toggling}>
-          {selectedOption||"0"}
-        </DropDownHeader2>
-        {isOpen && (
-          <DropDownListContainer2>
-            <DropDownList2>
-              {options.map(option => (
-                <ListItem2 onClick={onOptionClicked(option)} key={Math.random()}>
-                  {option}
-                </ListItem2>
-              ))}
-            </DropDownList2>
-          </DropDownListContainer2>
-        )}
-      </DropDownContainer2>
-      
-      
+        return (CalculateRank(home,away) <= CalculatePoint(referee))
+      }
+      else if(CalculateRank(home,away) === 60){
+        
+        if(CalculatePoint(referee) >= 60 && 75 > CalculatePoint(referee))
+        MatchImportance = 60;
+        MatchImportanceStr = "High Importance";
+        return (CalculatePoint(referee))
+      }
+      else if(CalculateRank(home,away) === 50){
+        if(CalculatePoint(referee) >= 50 && 60 > CalculatePoint(referee))
+        MatchImportance = 50;
+        MatchImportanceStr = "Medium Importance";
+        return (CalculatePoint(referee))
+      }
+      else{
+        if(CalculatePoint(referee) >= 0 && 50 > CalculatePoint(referee))
+        MatchImportance = 0;
+        MatchImportanceStr = "Low Importance";
+        return (CalculatePoint(referee))
+      }
+      }).map(createCard)} 
+      <FilterTitle>Match Importance = {MatchImportance} { MatchImportanceStr}</FilterTitle>
        <br></br>
        <br></br>
        <br></br>
@@ -100,8 +212,5 @@ export const ChooseReferee = (props) => {
       <Footer></Footer>
     
     </ContainerDiv>
-   
-   
-   
   );
 };
